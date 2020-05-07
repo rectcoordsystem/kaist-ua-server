459d5437757cf2660d79061885a79a6081871939
Seungho Kim <shokim01@gmail.com>
2020-05-08 02:38:36 +0900

    fix(cors):bad string formatting

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const host = process.env.SERVER_HOST
 const run = async () => {
   const app = new Koa();
   const corsOptions = {
-    origin: `{host}:3000`, // 허락하고자 하는 요청 주소
+    origin: `${host}:3000`, // 허락하고자 하는 요청 주소
     credentials: true, // true로 하면 설정한 내용을 response 헤더에 추가 해줍니다.
   };
 
