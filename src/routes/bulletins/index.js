4e11e85355f442232b5b5a72b66382ec8d9736c6
winningarc <shokim01@gmail.com>
2020-08-18 03:31:37 +0900

    feat(bulletin): add passport authentication

--- src/routes/bulletins/index.js
+++ src/routes/bulletins/index.js
@@ -1,10 +1,27 @@
 const Router = require("koa-router");
 const bulletins = new Router();
+const passport = require("koa-passport");
 const bulletinsCtrl = require("./bulletins.ctrl");
 
-bulletins.post("/", bulletinsCtrl.open);
-bulletins.get("/", bulletinsCtrl.list);
-bulletins.delete("/:id", bulletinsCtrl.close);
-bulletins.patch("/:id", bulletinsCtrl.reopen);
+bulletins.post(
+  "/",
+  passport.authenticate("token", { session: false }),
+  bulletinsCtrl.open
+);
+bulletins.get(
+  "/",
+  passport.authenticate("token", { session: false }),
+  bulletinsCtrl.list
+);
+bulletins.delete(
+  "/:id",
+  passport.authenticate("token", { session: false }),
+  bulletinsCtrl.close
+);
+bulletins.patch(
+  "/:id",
+  passport.authenticate("token", { session: false }),
+  bulletinsCtrl.reopen
+);
 
 module.exports = bulletins;
