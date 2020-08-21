d48f1f68f8b46a74f00a376e5b4929dddb3b0173
junsung.yoon <junsung.yoon@devsisters.com>
2020-08-21 17:46:19 +0900

    remove useless modules

--- src/routes/auth_archive/index.js
+++
@@ -1,9 +0,0 @@
-const Router = require('koa-router');
-const auth = new Router();
-const authCtrl = require('./auth.ctrl');
-
-auth.get('/login', authCtrl.login);
-auth.get('/register', authCtrl.register);
-auth.post('/signup', authCtrl.signUp);
-
-module.exports = auth;
